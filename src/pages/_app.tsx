ed8ad14cc0fa46a07c92bcef4a150132c4a8154c
devkeita <keita.mywork@gmail.com>
2021-08-26 21:17:21 +0900

    認証に必要なProviderを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
+import { RecoilRoot } from "recoil";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
+import AuthProvider from "../components/common/AuthProvider";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <RecoilRoot>
+      <AuthProvider>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </AuthProvider>
+    </RecoilRoot>
   );
 }
 export default MyApp;
